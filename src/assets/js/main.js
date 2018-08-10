eb934e8b31d328e4e5e9bea751d0038ef29399dc
Alejandro Vivas <alejandrojv17@gmail.com>
2018-08-10 14:35:52 -0400

    Maquetacion completa

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,5 @@
 //Delete this
-
+//import Vue from 'vue'
 
 $(document).ready(function(){
     console.log('Hello from main.js!');
@@ -10,4 +10,12 @@ $(document).ready(function(){
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
     })
+  })
+
+  var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Automotoras',
+      cars:[1,2,3,4,5,6,7,8,9,10,11]
+    }
   })
